e79e903ffcfc3ae2ac1b08a4bcc66cf96e558665
redietkuku <redietatsede@gmail.com>
2024-03-08 06:05:51 -0800

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    tot = 0
+    for x in range(len(sys.argv) - 1):
+        tot += int(sys.argv[x + 1])
+    print("{}".format(tot))
