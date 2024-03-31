74bb30d3579a7e8dfe918aa99539abd1fc9c26df
redietkuku <redietatsede@gmail.com>
2024-03-31 11:19:03 -0700

    error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Sends a request to a given URL and hanles HTTP errors.
+
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
