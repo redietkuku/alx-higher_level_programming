d5f0b614054b07ac4da62440148d7f7e2bed7541
redietkuku <redietatsede@gmail.com>
2024-03-31 12:40:02 -0700

    division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Returns the quotient of a and b"""
+
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except ZeroDivisionError:
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return div
