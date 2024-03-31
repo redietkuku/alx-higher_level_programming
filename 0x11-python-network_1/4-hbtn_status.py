cffd78527752967fffd6212ff77349578378df69
redietkuku <redietatsede@gmail.com>
2024-03-31 11:05:40 -0700

    fetches link

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Fetches the following link
+https://alx-intranet.hbtn.io/status.
+
+"""
+import requests
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
