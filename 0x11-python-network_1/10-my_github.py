3146a060e60c3fa19217d126661a6559b6a5cc5f
redietkuku <redietatsede@gmail.com>
2024-03-31 11:38:50 -0700

    MY GITHUB

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Uses the GitHub API to display a GitHub ID and uses
+basic authenticaton to access the ID
+
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
